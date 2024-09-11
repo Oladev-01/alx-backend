12b94dbe7de8bb1da056173988a50615db1f79e5
olalekan <lekanmojibola@gmail.com>
2024-09-11 21:39:11 +0100

    flask

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""flask instance"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+class Config:
+    """configure langs"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale"""
+    lang = request.args.get('locale')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return "request.accept_languages.best_match(app.config['LANGUAGES'])"
+
+
+@app.route("/")
+def index() -> str:
+    """return template"""
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
